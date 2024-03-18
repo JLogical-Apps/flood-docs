104c80b7b6ca9f1a4cc57172bd77de8b056f8260
jaboyc <jake@jlogical.com>
2024-03-18 15:10:16 -0400

    Initial commit.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import {DocsThemeConfig} from 'nextra-theme-docs';
+import Image from 'next/image';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+    logo: <>
+        <Image style={{display: 'inline'}} src="/flood.png"
+               alt="Flood Logo"
+               width={40} height={40}/>
+        {' '}Flood
+    </>,
+    docsRepositoryBase: 'https://github.com/JLogical-Apps/flood-docs',
+    footer: {
+        text: <span>
+            MIT {new Date().getFullYear()} ©{' '}
+            <a href="https://www.jlogical.com" target="_blank">
+              JLogical
+            </a>
+            .
+          </span>,
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s - Flood',
+        };
+    }
+};
 
-export default config
+export default config;
