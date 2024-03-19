3fa6d491fb8970a154784ff5ec1fad83b47a5f9d
jaboyc <jake@jlogical.com>
2024-03-19 16:56:59 -0400

    Add Google Analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        </>
+    );
 }
