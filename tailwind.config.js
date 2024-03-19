06c093b1504283cb7fe6d70c3eac8044888f4b08
jaboyc <jake@jlogical.com>
2024-03-19 16:30:16 -0400

    Add Pond and overview pages.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    content: [
+        './src/pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+        './src/components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
